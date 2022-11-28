52e7371249bd1fb17cf2f125d806621972c6db53
Christian Garcia <bones192nd@gmail.com>
2022-11-27 19:58:50 -0500

    commit tree 20f64052dae40edae88d8382a023bb1aca1ad7b1

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+function shout(string){
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    console.log(shout(string));
+}
+
+
+function logWhisper(string){
+    console.log(whisper(string))
+}
+
+function sayHiToHeadphonedRoommate(string){
+
+    switch(string){
+        case string.toLowerCase():
+            return 'I can\'t hear you!'
+
+        case string.toUpperCase():
+            return "YES INDEED!"
+
+        case "Let's have dinner together!":
+            return "I would love to!"
+    }
+
+}
